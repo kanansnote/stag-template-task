55ae4ef4f927f86d1a19140bc49b411f1e616987
kanan <kanansnote@gmail.com>
2024-05-08 14:35:20 +0400

    A few work with some section elements.

--- src/js/script.js
+++ src/js/script.js
@@ -23,9 +23,11 @@ const testimonialSwiper = new Swiper('.Testimonial', {
 });
 
 const gallerySwiper = new Swiper('.Gallery', {
-    slidesPerView: 'auto', // Adjust this value to control how many slides are visible at a time
-    navigation: {
-      nextEl: '.button-next',
-      prevEl: '.button-prev',
-    },
+  slidesPerView: 'auto',
+  loop: true, // Enable loop for endless scrolling
+  speed: 1500,
+  autoplay: {
+    delay: 0, // Set delay to 0 for immediate transition between slides
+    disableOnInteraction: false, // Continue autoplay after user interactions
+  },
 });
